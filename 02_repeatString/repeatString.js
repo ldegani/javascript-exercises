e824b7f91b8908b24f120801382ae04aa4d5273d
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-04-28 08:58:59 -0300

    Resolve JavaScript exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(name, num) {
+  if (num < 0) return 'ERROR';
+  let text = '';
+  let i = 0;
+  while (i < num) {
+    text += name;
+    i++;
+  }
+  return text;
 };
 
 // Do not edit below this line
