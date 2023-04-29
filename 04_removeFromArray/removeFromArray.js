af77cb2a54960be0e3c50e452f5c574833698701
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-04-29 18:24:16 -0300

    Resolve JavaScritp exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+  const array = arguments[0];
+  for (i = 1; i < arguments.length; i++) {
+    if (array.indexOf(arguments[i]) > -1) {
+      let index = array.indexOf(arguments[i]);
+      const removed = array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
