ccc307284ea07e77222a988ebc838c18b6c951ae
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-05-01 13:39:56 -0300

    Resolve JavaScript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  number = Number(number);
+  if (number < 0) return "OOPS";
+  let sumOld = 0;
+  let sum = 1;
+  for (let i = 1; i < number; i++) {
+    let total = sum;
+    sum = total + sumOld;
+    sumOld = total;
+  }
+  return sum;
 };
 
 // Do not edit below this line
