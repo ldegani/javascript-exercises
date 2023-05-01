4a3fd4cce55bf43c119ffdc249f2e8bf9aff94dc
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-05-01 10:16:50 -0300

    Resolve JavaScript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  text = text.toLowerCase().replace(/[^a-z]/gi,"");
+  let textReverse = text.split('').reverse().join('');
+  return (text === textReverse);
 };
 
 // Do not edit below this line
