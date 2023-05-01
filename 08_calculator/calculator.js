58965380945b865a29086d2215e5d1cdc7787ab9
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-05-01 07:28:22 -0300

    Resolve JavaScript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return sumArray = arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let num of arr){
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+  let total = 1;
+  let i = n1;
+	if (n1 === 0 || n1 === 1) return 1;
+  while (i > 1) {
+    total = total * i;
+
+    i--;
+  }
+  return total;
 };
 
 // Do not edit below this line
