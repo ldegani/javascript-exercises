6a7d9b4516669bfc7262fc51136aa59b71ecfc5f
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-05-24 14:08:16 -0300

    Resolve JavaScript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const older = people.reduce((oldest, currentPerson) => 
+    getDeath(oldest.yearOfDeath) - oldest.yearOfBirth > getDeath(currentPerson.yearOfDeath) - currentPerson.yearOfBirth ? 
+    oldest : currentPerson, 0
+  )
+  return older;
+};
 
+const getDeath = function(death) {
+  if (!death) {
+    const death = new Date().getFullYear();
+    return death
+  }
+  return death
 };
 
 // Do not edit below this line
